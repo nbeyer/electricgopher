8d81aa0d2ca3fddb2728bd011d5c855a35059061
Steve Butler <sbutler@ea.com>
2018-12-04 22:34:32 -0800

    Added implementation of Open Trunk/Frunk, refactored command response object to reflect commonality of type.

--- api/command.go
+++ api/command.go
@@ -18,19 +18,39 @@ type WakeUpOutput struct {
 	Response Vehicle `json:"response"`
 }
 
-func (c *Client) Unlock(id string) (*UnlockOutput, error) {
+func (c *Client) Unlock(id string) (*CommandResponse, error) {
 	c.logger.Debugf("electricgopher.api.Client.Unlock(%v): begin", id)
 	defer c.logger.Debugf("electricgopher.api.Client.Unlock(%v): end", id)
-	out := &UnlockOutput{}
+	out := &ResponseEnvelope{}
 	err := c.doPost(fmt.Sprintf("/api/1/vehicles/%s/command/door_unlock", id), out, bytes.NewBufferString(""))
-	return out, err
+	return &out.Response, err
 }
 
-type UnlockOutput struct {
-	Response UnlockPayload `json:"response"`
+func (c *Client) OpenFrunk(id string) (*CommandResponse, error) {
+	c.logger.Debugf("electricgopher.api.Client.OpenFrunk(%v): begin", id)
+	defer c.logger.Debugf("electricgopher.api.Client.OpenFrunk(%v): end", id)
+	return c.actuateTrunk(id, "front")
 }
 
-type UnlockPayload struct {
+func (c *Client) OpenTrunk(id string) (*CommandResponse, error) {
+	c.logger.Debugf("electricgopher.api.Client.OpenTrunk(%v): begin", id)
+	defer c.logger.Debugf("electricgopher.api.Client.OpenTrunk(%v): end", id)
+	return c.actuateTrunk(id, "rear")
+}
+
+func (c *Client) actuateTrunk(id string, which string) (*CommandResponse, error) {
+	c.logger.Debugf("electricgopher.api.Client.OpenFrunk(%v): begin", id)
+	defer c.logger.Debugf("electricgopher.api.Client.OpenFrunk(%v): end", id)
+	out := &ResponseEnvelope{}
+	err := c.doPost(fmt.Sprintf("/api/1/vehicles/%s/command/actuate_trunk", id), out, bytes.NewBufferString(fmt.Sprintf("which_trunk=%s", which)))
+	return &out.Response, err
+}
+
+type ResponseEnvelope struct {
+	Response CommandResponse `json:"response"`
+}
+
+type CommandResponse struct {
 	Reason string `json:"reason"`
 	Result bool   `json:"result"`
 }
